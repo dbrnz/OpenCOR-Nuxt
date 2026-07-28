dabe970a5a03ebb23f72d7c2970fc4a8d01f348f
David Brooks <dbrnz@users.noreply.github.com>
2026-07-28 21:57:51 +1200

    We need the global `index.ts`.

---
+++ index.ts
@@ -0,0 +1,82 @@
+export type OpenCORTheme = 'light' | 'dark' | 'system';
+
+export interface IOpenCORProps {
+  omex?: string | Uint8Array;
+  theme?: OpenCORTheme;
+}
+
+export interface IOpenCORExpose {
+  addExternalData: (csv: string, voiExpression: string | undefined, modelParameters: string[]) => void;
+  trackSimulationData: (modelParameters: string[]) => void;
+  untrackSimulationData: (modelParameters: string[]) => void;
+  untrackAllSimulationData: () => void;
+}
+
+export interface IOpenCOREmits extends /* @vue-ignore */ Record<string, unknown[]> {
+  externalData: [IOpenCORExternalDataEvent];
+  file: [IOpenCORFileEvent];
+  simulationData: [IOpenCORSimulationDataEvent];
+}
+
+export interface IOpenCORSimulationDataValue {
+  data: Float64Array;
+  unit: string;
+}
+
+// External data events.
+
+export interface IOpenCORExternalDataAddedEvent {
+  type: 'added';
+  csv: string;
+  issues: string[];
+}
+
+export interface IOpenCORExternalDataIssueEvent {
+  type: 'issue';
+  csv: string;
+  issues: string[];
+}
+
+export type IOpenCORExternalDataEvent = IOpenCORExternalDataAddedEvent | IOpenCORExternalDataIssueEvent;
+
+// File events.
+
+export interface IOpenCORFileOpenedEvent {
+  type: 'opened';
+  filePath: string;
+  issues: string[];
+}
+
+export interface IOpenCORFileClosedEvent {
+  type: 'closed';
+  filePath: string;
+  issues: string[];
+}
+
+export interface IOpenCORFileIssueEvent {
+  type: 'issue';
+  filePath: string;
+  issues: string[];
+}
+
+export type IOpenCORFileEvent = IOpenCORFileOpenedEvent | IOpenCORFileClosedEvent | IOpenCORFileIssueEvent;
+
+// Simulation data events.
+
+export type OpenCORSimulationData = Record<string, IOpenCORSimulationDataValue>;
+
+export interface IOpenCORSimulationDataUpdatedEvent {
+  type: 'updated';
+  simulationData: OpenCORSimulationData;
+  issues: string[];
+}
+
+export interface IOpenCORSimulationDataIssueEvent {
+  type: 'issue';
+  simulationData: OpenCORSimulationData;
+  issues: string[];
+}
+
+export type IOpenCORSimulationDataEvent = IOpenCORSimulationDataUpdatedEvent | IOpenCORSimulationDataIssueEvent;
+
+export { default, default as OpenCOR } from './app/components/OpenCOR.vue';
