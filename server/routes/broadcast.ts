0f59f9608d30f52ae2ae502cc380a2c9aefafea5
David Brooks <dbrnz@users.noreply.github.com>
2026-08-02 07:58:50 +1200

    We no longer need to use Redis so remove code.

--- server/routes/broadcast.ts
+++ server/routes/broadcast.ts
@@ -1,24 +1,5 @@
-import { getRedisClient } from '#server/utils/redis'
-
-export const OPENCOR_CHANNEL = 'opencor'
-
 // Track locally connected WebSocket clients on this server instance
 const localPeers = new Set<any>()
-
-// Initialize the Redis Subscription listener
-const subClient = getRedisClient('sub')
-const pubClient = getRedisClient('pub')
-
-subClient.subscribe(OPENCOR_CHANNEL)
-subClient.on('message', (channel, message) => {
-    if (channel === OPENCOR_CHANNEL) {
-        // Broadcast the message received from Redis to all locally connected users
-        const parsedMessage = JSON.parse(message)
-        for (const peer of localPeers) {
-            peer.send(parsedMessage)
-        }
-    }
-})
 
 export default defineWebSocketHandler({
     open(peer) {
@@ -34,9 +15,11 @@ export default defineWebSocketHandler({
             timestamp: Date.now()
         }
 
-        // Publish message to Redis instead of sending it locally.
-        // This scales across multiple server instances perfectly.
-        await pubClient.publish(OPENCOR_CHANNEL, JSON.stringify(payload))
+        // Broadcast the message to all locally connected users
+        for (const peer of localPeers) {
+            // but don't send back to ourself...
+            peer.send(payload)
+        }
     },
 
     close(peer) {
