d89affb723d83b44ad2164bc0638b64cf318cbd4
David Brooks <dbrnz@users.noreply.github.com>
2026-08-03 16:28:58 +1200

    Get a directory listing page working.

--- app/pages/pvLazy3.vue
+++ app/pages/pvLazy3.vue
@@ -1,4 +1,5 @@
 <template>
+<ClientOnly>
     <Tree :value="nodes" @node-expand="onNodeExpand" loadingMode="icon" class="w-full md:w-120"
         selectionMode="single"
 
@@ -20,19 +21,16 @@
                 @dblclick="onNodeDblClick($event, node)"
                 :id="node.key"
                 class="cursor-pointer w-full display-block"
-            >
-                XX {{ node.label }}
-            </span>
+            > {{ node.label }} </span>
         </template>
     </Tree>
+</ClientOnly>
 </template>
 
-<script setup>
-
+<script setup lang="ts">
 
 //        @nodeSelect="onNodeSelected"
 //        v-model:selectionKeys="selectedKeys"
-
 
 import ChevronDown from '@primeicons/vue/chevron-down';
 import ChevronRight from '@primeicons/vue/chevron-right';
@@ -41,14 +39,41 @@ import Folder from '@primeicons/vue/folder';
 import FolderOpen from '@primeicons/vue/folder-open';
 import Spinner from '@primeicons/vue/spinner';
 
-const nodes = ref(null);
+import type { TreeNode } from 'primevue/treenode'
 
-onMounted(() => {
-    nodes.value = initiateNodes();
+const nodes = ref<TreeNode[]>([]);
 
-    setTimeout(() => {
-        nodes.value.map((node) => (node.loading = false));
-    }, 700);
+
+//import type { FSEntry } from '#server/utils/fs'
+export interface FsEntry {
+    name: string
+    path: string
+    isDirectory: boolean
+    isGitRepo: boolean
+    branch?: string
+    isIgnored?: boolean
+    size?: number
+    modified?: string
+}
+
+function buildDirectoryTree(dirList?: FsEntry[]): TreeNode[] {
+    const tree: TreeNode[] = []
+    if (dirList) {
+        for (const entry of dirList) {
+            tree.push({
+                key: entry.path,
+                label: entry.name,
+                data: entry.path,
+                leaf: !entry.isDirectory
+            })
+        }
+    }
+    return tree
+}
+
+onMounted(async () => {
+  const dirList = await $fetch<FsEntry[]>('/api/dir')
+  nodes.value = buildDirectoryTree(dirList)
 });
 
 const onNodeSelected = (event, node) => {
@@ -66,66 +91,28 @@ const onNodeDblClick = (event, node) => {
     console.log('Dbl click', event, node)
 }
 
-const selectedKeys = ref(null);
+const selectedKeys = ref<Record<string, boolean>>(null);
 
 // Toggle function
-const toggleNodeSelection = (nodeKey) => {
-    if (selectedKeys.value[nodeKey]) {
+const toggleNodeSelection = (key: string) => {
+    if (selectedKeys.value[key]) {
         // If already selected, remove it to unselect
-        delete selectedKeys.value[nodeKey];
+        delete selectedKeys.value[key]
     } else {
         // If not selected, add it to select
-        selectedKeys.value[nodeKey] = true;
+        selectedKeys.value[key] = true
     }
     // Force Vue to react to the object property modification
     selectedKeys.value = { ...selectedKeys.value };
 };
 
-const onNodeExpand = (node) => {
+
+async function onNodeExpand(node: TreeNode) {
     if (!node.children) {
-        node.loading = true;
-
-        setTimeout(() => {
-            let _node = { ...node };
-
-            _node.children = [];
-
-            for (let i = 0; i < 3; i++) {
-                _node.children.push({
-                    key: node.key + '-' + i,
-                    label: 'Lazy ' + node.label + '-' + i
-                });
-            }
-
-            let _nodes = { ...nodes.value };
-
-            _nodes[parseInt(node.key, 10)] = { ..._node, loading: false };
-
-            nodes.value = _nodes;
-        }, 500);
+        node.loading = true
+        const dirList = await $fetch<FsEntry[]>(`/api/dir/${node.data}`)
+        node.children = buildDirectoryTree(dirList)
+        node.loading = false
     }
-};
-
-const initiateNodes = () => {
-    return [
-        {
-            key: '0',
-            label: 'Node 0',
-            leaf: false,
-            loading: true
-        },
-        {
-            key: '1',
-            label: 'Node 1',
-            leaf: false,
-            loading: true
-        },
-        {
-            key: '2',
-            label: 'Node 2',
-            leaf: false,
-            loading: true
-        }
-    ];
-};
+}
 </script>
